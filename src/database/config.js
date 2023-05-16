afae283fd0d4e426f05b686e6aa8abb1ad6e98b9
Ephrat Negussie Belaineh <113403115+EphratB@users.noreply.github.com>
2023-05-16 19:48:33 -0400

    added vercel.json

--- src/database/config.js
+++ src/database/config.js
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0IPz05yfrb89SQRk9M21B-OxIIYzorGs",
-  authDomain: "addiscinema-6d1aa.firebaseapp.com",
-  projectId: "addiscinema-6d1aa",
-  storageBucket: "addiscinema-6d1aa.appspot.com",
-  messagingSenderId: "1078145706966",
-  appId: "1:1078145706966:web:f963ff33b6c44086abd277",
+  apiKey: "AIzaSyBtrLfjENjwEu6c6EsVVkOYupwMKqPXQMY",
+  authDomain: "addiscinemaapp.firebaseapp.com",
+  projectId: "addiscinemaapp",
+  storageBucket: "addiscinemaapp.appspot.com",
+  messagingSenderId: "756936030571",
+  appId: "1:756936030571:web:7f97b99ff7d103396f342e",
+  measurementId: "G-ZMM1Q37L92",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
